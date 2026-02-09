05104bb598bccab1a2c001d688df14fc0b173e20
Hetsi Tewar <tewarhetsi@gmail.com>
2026-02-09 10:17:58 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,72 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+// resize canvas
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+// particle array
+let particles = [];
+const PARTICLE_COUNT = 800;
+
+// particle class
+class Particle {
+    constructor() {
+        this.x = Math.random() * canvas.width;
+        this.y = Math.random() * canvas.height;
+        this.size = 2;
+        this.speedX = (Math.random() - 0.5) * 1.5;
+        this.speedY = (Math.random() - 0.5) * 1.5;
+    }
+
+    update() {
+        this.x += this.speedX;
+        this.y += this.speedY;
+
+        // bounce from edges
+        if (this.x < 0 || this.x > canvas.width) {
+            this.speedX *= -1;
+        }
+        if (this.y < 0 || this.y > canvas.height) {
+            this.speedY *= -1;
+        }
+    }
+
+    draw() {
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+        ctx.fillStyle = "white";
+        ctx.fill();
+    }
+}
+
+// create particles
+function initParticles() {
+    particles = [];
+    for (let i = 0; i < PARTICLE_COUNT; i++) {
+        particles.push(new Particle());
+    }
+}
+
+// animation loop
+function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    for (let i = 0; i < particles.length; i++) {
+        particles[i].update();
+        particles[i].draw();
+    }
+
+    requestAnimationFrame(animate);
+}
+
+// handle resize
+window.addEventListener("resize", () => {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+});
+
+// start
+initParticles();
+animate();
 
