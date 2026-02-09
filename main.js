c55b273392a9348eb1e9944092a04c96ee9d5ab9
Hetsi Tewar <tewarhetsi@gmail.com>
2026-02-09 10:29:23 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,35 +1,32 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
-// resize canvas
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-// particle array
 let particles = [];
-const PARTICLE_COUNT = 800;
+const PARTICLE_COUNT = 900;
+
+// mouse object
+const mouse = {
+    x: null,
+    y: null,
+    radius: 120
+};
+
+window.addEventListener("mousemove", (event) => {
+    mouse.x = event.x;
+    mouse.y = event.y;
+});
 
 // particle class
 class Particle {
     constructor() {
         this.x = Math.random() * canvas.width;
         this.y = Math.random() * canvas.height;
+        this.baseX = this.x;
+        this.baseY = this.y;
         this.size = 2;
-        this.speedX = (Math.random() - 0.5) * 1.5;
-        this.speedY = (Math.random() - 0.5) * 1.5;
-    }
-
-    update() {
-        this.x += this.speedX;
-        this.y += this.speedY;
-
-        // bounce from edges
-        if (this.x < 0 || this.x > canvas.width) {
-            this.speedX *= -1;
-        }
-        if (this.y < 0 || this.y > canvas.height) {
-            this.speedY *= -1;
-        }
     }
 
     draw() {
@@ -37,6 +34,20 @@ class Particle {
         ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
         ctx.fillStyle = "white";
         ctx.fill();
+    }
+
+    update() {
+        let dx = mouse.x - this.x;
+        let dy = mouse.y - this.y;
+        let distance = Math.sqrt(dx * dx + dy * dy);
+
+        if (distance < mouse.radius) {
+            this.x -= dx / 10;
+            this.y -= dy / 10;
+        } else {
+            this.x += (this.baseX - this.x) / 20;
+            this.y += (this.baseY - this.y) / 20;
+        }
     }
 }
 
@@ -52,21 +63,21 @@ function initParticles() {
 function animate() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    for (let i = 0; i < particles.length; i++) {
-        particles[i].update();
-        particles[i].draw();
-    }
+    particles.forEach(particle => {
+        particle.update();
+        particle.draw();
+    });
 
     requestAnimationFrame(animate);
 }
 
-// handle resize
+// resize support
 window.addEventListener("resize", () => {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
+    initParticles();
 });
 
 // start
 initParticles();
 animate();
-
